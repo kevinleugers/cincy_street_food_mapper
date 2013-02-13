533abfddc6dbb7b2b00f53a5696b7eb2e80ac142
kevindigital84 <kevin.leugers@gmail.com>
2013-02-13 16:17:46 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CincyStreetFoodMapper::Application.config.secret_token = 'edae37f6b80332cdaad510fdba0bccb2ab4790e3e775aaa86941d728eecca9de582571d6765513eb3dcbd3d96e0ee75c25718ba288ea2e50d2230e74a264ff00'

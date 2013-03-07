8a26b006ff4c64afafe16fa0d7dda77887ef435e
kevindigital84 <kevin.leugers@gmail.com>
2013-03-06 20:05:11 -0500

    fixed map loading inside div for styling

--- app/assets/javascripts/create_map.js
+++ app/assets/javascripts/create_map.js
@@ -1,17 +1,20 @@
 var map;
-var cincy = new google.maps.LatLng(39.12970142, -84.50935856);
+
 var marker;
-var mapOptions = {
+
+tweets.push({"geo": {"coordinates": [39.12970142, -84.50935856]}});
+
+function initialize() {
+        var cincy = new google.maps.LatLng(39.12970142, -84.50935856);
+        var mapOptions = {
           center: cincy,
           zoom: 13,
           mapTypeId: google.maps.MapTypeId.ROADMAP
         };
-tweets.push({"geo": {"coordinates": [39.12970142, -84.50935856]}});
-
-function initialize() {
         var map = new google.maps.Map(document.getElementById("map_canvas"),
             mapOptions);
         $.each(tweets, function(k,v) {
+
           mark = new google.maps.Marker({
             map: map,
             position: new google.maps.LatLng(v.geo.coordinates[0], v.geo.coordinates[1]),
@@ -19,5 +22,13 @@ function initialize() {
           });
         });
 
-      };
+      }
 
+function loadScript() {
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = "http://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyDin_8yEPNrV3c4CQQOBzyW9oZ3GWXr-y4&sensor=true&callback=initialize";
+  document.body.appendChild(script);
+}
+
+window.onload = loadScript;
